43fa44b72782da1b9da6c91bd00ca8c88c335ffd
antonio98pt <antonio98pt@gmail.com>
2020-11-24 18:34:34 +0100

    Error 6 actualizado

--- src/compruebaYaDeclarados.java
+++ src/compruebaYaDeclarados.java
@@ -19,39 +19,67 @@ import java.util.Map;
          no error
 */
 
-public class compruebaYaDeclarados  {
+public class compruebaYaDeclarados extends AnasintBaseVisitor<Object>  {
 
-    public  Map<String,Integer> almacen = new HashMap<String,Integer>();
-
-    public Boolean compruebaYaDeclarados(String ident) {
-        Integer aux = almacen.get(ident);
+    public  Map<String,String> almacen = new HashMap<String,String>();
+    
+    public Boolean compruebaYaDeclarados(String ident) { //Falta añadir el almacen que la función debe mirar
+        String aux = almacen.get(ident);
         Boolean res = false;
         if (aux == null) {
-
+            System.out.println("NO ERROR");
         } else {
             res = true;
+            System.out.println("Error: Variable ya declarada");
         }
         return res;
     }
 
-    public Integer visitTipos_elementales(Anasint.Tipos_elementalesContext ctx, String var) {
-        Boolean varLibre=compruebaYaDeclarados(var);
-        if(varLibre==true){
-            almacen.put(var,new Integer(ctx.getStart().getType()));
-        }else{
-            System.out.println("Error: Variable ya declarada");
-        }
-        return null;
+    public String visitDeclaracion_variables(Anasint.Declaracion_variablesContext ctx){
+
+        return ctx.getText();
     }
 
-    public Integer visitTipos_no_elementales(Anasint.Tipos_no_elementalesContext ctx, String var) {
-        Boolean varLibre=compruebaYaDeclarados(var);
-        if(varLibre==true){
-            almacen.put(var,new Integer(ctx.getStart().getType()));
-        }else{
-            System.out.println("Error: Variable ya declarada");
+    public Map<String, String> visitIdentificador(Anasint.IdentificadorContext ctx) {
+        return (Map<String, String>) visit(ctx.getChild(0));
+    }
+
+    public Map<String, String> visitElementales(Anasint.ElementalesContext ctx) {
+        Map<String, String> varYTipo = new HashMap<>();
+        String tipo = ctx.getChild(ctx.children.size()-1).getText(); //el último hijo es tipo_elementales o tipo_no_elementales
+        for(int i = 0; i<ctx.children.size()-1;i=i+2) { //de dos en dos para saltarme , y :
+            String r = ctx.getChild(i).getText();
+            Boolean aux = compruebaYaDeclarados(r);
+            if(aux==false) {
+                varYTipo.put(r,tipo);
+            }
         }
-        return null;
+        return varYTipo;
+    }
+
+    public Map<String, String> visitSecuencias(Anasint.SecuenciasContext ctx) {
+        Map<String, String> varYTipo = new HashMap<>();
+        String tipo = ctx.getChild(2).getText();
+        String r = ctx.getChild(0).getText();
+        Boolean aux = compruebaYaDeclarados(r);
+        if(aux==false) {
+            varYTipo.put(r,tipo);
+        }
+        return varYTipo;
+    }
+
+    public String visitTipos(Anasint.TiposContext ctx) {
+        return (String) visit(ctx.getChild(0));
+    }
+
+
+    public String visitTipos_elementales(Anasint.Tipos_elementalesContext ctx) {
+        return ctx.getText();
+    }
+
+
+    public String visitTipos_no_elementales(Anasint.Tipos_no_elementalesContext ctx) {
+        return ctx.getText();
     }
 
 
