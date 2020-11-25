e6fab950ca17b6ffd4512d12bbebf92ba9edff63
EnriqueFdez03 <enrique.fdezco@gmail.com>
2020-11-25 17:23:25 +0100

    anasem corregido

--- PL/src/anasem.java
+++ PL/src/anasem.java
@@ -5,20 +5,15 @@ import java.util.Map;
 
 public class anasem extends AnasintBaseListener{
     public static Map<String,Map<String, Map<String, String>>> almacenF = new HashMap<>();
-    public static Map<String,Map<String, Map<String, String>>> almacenGlobal = new HashMap<>();
+    public static Map<String,String> almacenGlobal = new HashMap<>();
 
     public anasem(ParseTree tree) {
         AlmacenGlobal g = new AlmacenGlobal();
         AlmacenFunciones v = new AlmacenFunciones();
-        this.almacenGlobal = (Map<String, Map<String, Map<String, String>>>) g.visit(tree);
-        System.out.println(this.almacenGlobal);
+        this.almacenGlobal = (Map<String,String>) g.visit(tree);
         this.almacenF = (Map<String, Map<String, Map<String, String>>>) v.visit(tree);
         CompruebaDevolución c = new CompruebaDevolución(tree);
         c.visit(tree);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("global: " + almacenGlobal);
     }
 
 }
